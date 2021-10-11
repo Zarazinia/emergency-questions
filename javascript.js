82953412da97b9c2dafcdb7afe887e54141d2129
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2021-10-11 16:09:40 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ var quotes = [
   'What\'s the worst thing you\'ve ever eaten?',
   'What\'s the best thing you\'ve ever eaten?',
 ]
+function newQuote() {
+  var randomNumber = Math.floor(Math.random)) * (quotes.length)); 
+  document.GetElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
