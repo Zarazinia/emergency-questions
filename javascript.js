b741af5a1e3f074948410b3c5f2bf7473dc20dcd
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2021-10-11 16:10:57 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 var quotes = [
   'What\'s the worst thing you\'ve ever eaten?',
   'What\'s the best thing you\'ve ever eaten?',
+  '3',
+  '4',
+  '5',
+  '6',
 ]
 function newQuote() {
   var randomNumber = Math.floor(Math.random)) * (quotes.length)); 
