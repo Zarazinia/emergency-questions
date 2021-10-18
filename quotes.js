fa2d50552e752acae7bd5de3651104586661a429
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2021-10-18 14:37:48 +0100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,5 +1,7 @@
 var quotes = [
  
+    '\"What sport would be the funniest to add a mandatory amount of alcohol to?',
+    '\"What would be the coolest animal to scale up to the size of a horse?',
     '\"If you were wrongfully put into an insane asylum, how would you convince them that you’re actually sane and not just pretending to be sane?',
     '\"If you were arrested with no explanation, what would your friends and family assume you had done?'
     
