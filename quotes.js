94a29669116af990592f00598a76dba0c434030a
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2021-12-14 16:31:36 +0000

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,5 +1,6 @@
 var quotes = [
  
+    'What\'s the most recent joke you\'ve heard?',
     'What bin do you need to put out this week?',
     'Who is the best super villain of all time?',
     'What\’s your favorite thing you own, and why?',
