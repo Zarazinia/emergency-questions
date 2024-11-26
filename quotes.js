45575fac58a307c1f1ebb4662229d6814691da5d
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2024-11-26 15:22:14 +0000

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -100,7 +100,8 @@ var quotes = [
  'Why do we get annoyed?',
  'If you could do any job, what would it be?',
  'If you could bring one plant or creature back from extinction, what would you choose?',
- 'What are your favourite and least favourite words?'
+ 'What are your favourite and least favourite words?',
+ 'What\'s your go-to karaoke song?'
 ]
 
 function newQuote(){
