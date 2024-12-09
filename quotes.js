f23cd160a5d4db981975a18659873a2144ee9422
Zarazinia <47556033+Zarazinia@users.noreply.github.com>
2024-12-09 11:12:56 +0000

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -117,7 +117,8 @@ var quotes = [
  'What\'s the best thing you\'ve ever found?',
  'What\'s the best free thing that you get?',
  'Which non-potato crisp is the best?',
- 'Who is the first person you remember fancying?'
+ 'Who is the first person you remember fancying?',
+ 'What song would you like to replace the National Anthem?'
 ]
 
 function newQuote(){
